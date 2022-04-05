5a47418d133470b4932a532d8d1f2e8fb6fe45ab
Karol Kisinski <karol.kisinski@wp.pl>
2022-04-05 23:40:49 +0200

    Added CoreData, created CDManager, added view to content view

--- Car Rental/ContentView.swift
+++ Car Rental/ContentView.swift
@@ -8,14 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let coreDM: CoreDataManager
+    
+    @State private var carBrand: String = ""
+    @State private var carModel: String = ""
+    @State private var carPicName: String = ""
+    
+    @State private var cars: [Car] = [Car]()
+    
+    private func populateCars() {
+        cars = coreDM.getAllCars()
+    }
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                TextField("Enter brand", text: $carBrand)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal)
+                TextField("Enter model", text: $carModel)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal)
+                TextField("Enter pic name", text: $carPicName)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal)
+                Button(action: {
+                    coreDM.saveCar(brand: carBrand, model: carModel, pic_name: carPicName)
+                    populateCars()
+                }, label: {
+                    Text("save".uppercased())
+                        .foregroundColor(.white)
+                        .frame(height: 45)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.accentColor)
+                        .cornerRadius(10)
+                        .font(.headline)
+                        .padding(.horizontal)
+                })
+                
+                List {
+                    
+                    ForEach(cars, id: \.self) { car in
+                        Text(car.model ?? "")
+                    }.onDelete(perform: { indexSet in
+                        indexSet.forEach { index in
+                            let car = cars[index]
+                            //delete using CDM
+                            coreDM.deleteCar(car: car)
+                            populateCars()
+                        }
+                    })
+                }
+                
+                Spacer()
+            }.padding()
+            .navigationTitle("Cars")
+            
+            .onAppear(perform: {
+                cars = coreDM.getAllCars()
+            })
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(coreDM: CoreDataManager())
     }
 }
---
+++ Car Rental/Managers/CoreDataManager.swift
@@ -0,0 +1,60 @@
+//
+//  CoreDataManager.swift
+//  Car Rental
+//
+//  Created by karol on 05/04/2022.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager {
+    
+    let persistenContainer: NSPersistentContainer
+    
+    init() {
+        persistenContainer = NSPersistentContainer(name: "CoreDataModel")
+        persistenContainer.loadPersistentStores { (description, error) in
+            if let error = error {
+                fatalError("Core Data Store failed \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func getAllCars() -> [Car] {
+        
+        let fetchRequest: NSFetchRequest<Car> = Car.fetchRequest()
+        do {
+            return try persistenContainer.viewContext.fetch(fetchRequest)
+        } catch {
+            return []
+        }
+    }
+    
+    func saveCar(brand: String, model: String, pic_name: String) {
+        
+        let car = Car(context: persistenContainer.viewContext)
+        car.brand = brand
+        car.model = model
+        car.pic_name = pic_name
+        
+        do {
+            try persistenContainer.viewContext.save()
+        } catch {
+            print("Failed to save car \(error)")
+        }
+    }
+    
+    func deleteCar(car: Car) {
+        persistenContainer.viewContext.delete(car)
+        
+        do {
+            try persistenContainer.viewContext.save()
+        } catch {
+            persistenContainer.viewContext.rollback()
+            print("Failed to save context \(error)")
+        }
+    }
+    
+    
+}
