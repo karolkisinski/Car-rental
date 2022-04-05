c24570d681d7144e7bdad4a9f20df74ec5486c2f
Karol Kisinski <karol.kisinski@wp.pl>
2022-04-05 22:33:12 +0200

    Initial Commit

---
+++ Car Rental/Car_RentalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Car_RentalApp.swift
+//  Car Rental
+//
+//  Created by karol on 05/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Car_RentalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Car Rental/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Car Rental
+//
+//  Created by karol on 05/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
